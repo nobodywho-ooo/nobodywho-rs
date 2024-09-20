e487f1ce8e8ac40bddb461c5ea92c147986cd3d8
AsbjornOlling <asbjornolling@gmail.com>
2024-09-20 14:21:25 +0200

    run() automatically if the user forgot

--- godot/prompt.gd
+++ godot/prompt.gd
@@ -3,7 +3,6 @@ extends NobodyPrompt
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.model_node.run()
 	prompt("Hello, my name is")
 	pass # Replace with function body.
 
--- nobody/src/lib.rs
+++ nobody/src/lib.rs
@@ -21,7 +21,7 @@ struct NobodyModel {
     #[export]
     seed: u32,
 
-    model_actor: Option<ModelActor>
+    model_actor: Option<ModelActor>,
 }
 
 #[godot_api]
@@ -30,7 +30,11 @@ impl INode for NobodyModel {
         // default values to show in godot editor
         let model_path = "model.bin".into();
         let seed = 1234;
-        Self { model_path, seed, model_actor: None }
+        Self {
+            model_path,
+            seed,
+            model_actor: None,
+        }
     }
 }
 
@@ -38,7 +42,8 @@ impl INode for NobodyModel {
 impl NobodyModel {
     #[func]
     fn run(&mut self) {
-        let mut model_actor = ModelActor::from_model_path(self.model_path.to_string()).with_seed(self.seed);
+        let mut model_actor =
+            ModelActor::from_model_path(self.model_path.to_string()).with_seed(self.seed);
         model_actor.run();
         self.model_actor = Some(model_actor);
     }
@@ -61,7 +66,12 @@ struct NobodyPrompt {
 impl INode for NobodyPrompt {
     fn init(base: Base<Node>) -> Self {
         let (tx, rx) = std::sync::mpsc::channel();
-        Self { model_node: None, rx, tx, base, }
+        Self {
+            model_node: None,
+            rx,
+            tx,
+            base,
+        }
     }
 
     fn physics_process(&mut self, _delta: f64) {
@@ -75,17 +85,26 @@ impl INode for NobodyPrompt {
 #[godot_api]
 impl NobodyPrompt {
     #[func]
-    fn prompt(&self, prompt: String) {
-        match &self.model_node {
-            Some(gd_model_node) => {
-                let nobody_model: GdRef<NobodyModel> = gd_model_node.bind();
-                match &nobody_model.model_actor {
-                    Some(model_actor) => model_actor.get_completion(prompt, self.tx.clone()),
-                    None => panic!("you must run run() on the model first"),
-                }
-            },
+    fn prompt(&mut self, prompt: String) {
+        match self.model_node {
+            Some(ref mut gd_model_node) => {
+                let mut nobody_model: GdMut<NobodyModel> = gd_model_node.bind_mut();
+                let model_actor = match &nobody_model.model_actor {
+                    Some(model_actor) => model_actor,
+                    None => {
+                        godot_warn!("Model was not loaded yet. Loading now... Call the .run() method on NobodyModel to control when to load the model.");
+                        nobody_model.run();
+                        if let Some(model_actor) = &nobody_model.model_actor {
+                            model_actor
+                        } else {
+                            panic!("Unexpected: nobody_model.model_actor is still None after calling run()");
+                        }
+                    }
+                };
+                model_actor.get_completion(prompt, self.tx.clone());
+            }
             None => {
-                panic!("you must set a model node first");
+                godot_error!("you must set a model node first");
             }
         }
     }
