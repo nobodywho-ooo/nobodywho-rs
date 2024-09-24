6362f0cac0982773898ccac3fdbb6ea4b4e0191b
AsbjornOlling <asbjornolling@gmail.com>
2024-09-24 15:24:07 +0200

    expand demo game: call run(), and add a spinner to show blocking calls

--- godot/prompt.gd
+++ godot/prompt.gd
@@ -3,13 +3,8 @@ extends NobodyPrompt
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	run()
 	prompt("Hello, my name is")
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 
 func _on_completion_updated(text):
-	print(text)
+	print("Got new text from godot signal: " + text)
