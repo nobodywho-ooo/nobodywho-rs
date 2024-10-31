a8fad343988510a93a2cb2841028492f75a5533e
AsbjornOlling <asbjornolling@gmail.com>
2024-10-31 15:26:13 +0100

    get rid of custom character/ai names, to more easily conform to prompt templates

--- godot/prompt.gd
+++
@@ -1,9 +0,0 @@
-extends NobodyWhoPromptCompletion
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	run()
-	prompt("Hello, my name is")
-
-func _on_completion_updated(text):
-	print(text)
