faaa058c655ec99102ac95c1cb1dda046d847079
volesen <vincolesen@gmail.com>
2024-11-16 16:31:19 +0100

    Port example from `sqlite-vec` to Godot

--- example-game/nobody_who_db.gd
+++ example-game/nobody_who_db.gd
@@ -3,13 +3,32 @@ extends NobodyWhoDB
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    self.open_db();
+	open_db()
 
-    print(self.query("SELECT sqlite_version(), vec_version();", []))
+	print(query("SELECT sqlite_version(), vec_version();", []))
 
-    print(self.execute("CREATE TABLE IF NOT EXISTS texts (id INTEGER PRIMARY KEY, name TEXT);", []));
-    print(self.execute("INSERT INTO texts (name) VALUES (\"Hello, World!\"), (\"Goodbye, World!\");", []));
+	execute("CREATE VIRTUAL TABLE vec_items USING vec0(embedding float[4]);", []);
 
-    print(self.query("SELECT * FROM texts;", []));
+	var items = [
+		[1, PackedFloat32Array([0.1, 0.1, 0.1, 0.1])],
+		[2, PackedFloat32Array([0.2, 0.2, 0.2, 0.2])],
+		[3, PackedFloat32Array([0.3, 0.3, 0.3, 0.3])],
+		[4, PackedFloat32Array([0.4, 0.4, 0.4, 0.4])],
+		[5, PackedFloat32Array([0.5, 0.5, 0.5, 0.5])],
+	]
 
-    self.close_db();
+	for item in items:
+		execute("INSERT INTO vec_items(rowid, embedding) VALUES (?, ?);", item);
+
+	var knn_query = """
+		SELECT rowid, distance 
+		FROM vec_items 
+		WHERE embedding MATCH ?1 
+		ORDER BY distance 
+		LIMIT 3;
+	"""
+	var search_vector = PackedFloat32Array([0.3, 0.3, 0.3, 0.3]);
+
+	print(query(knn_query, [search_vector]))
+
+	self.close_db();
